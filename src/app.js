283a87f2e4643313cfbfaa66aadfdf8a0d2bf978
Maikel Cardenas <163206166+maikelcaardenas@users.noreply.github.com>
2024-05-20 21:03:46 +0000

    arreglos-finales-proyecto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let dominios = [];
+const pronoun = ['the', 'our'];
+const adj = ['awesome', 'tiny', 'prime', 'max'];
+const noun = ['wiki', 'video', 'plus', 'hub'];
+const ext = ['.com', '.org', '.es', '.io', '.net'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+            for (let l = 0; l < ext.length; l++) {
+                dominios.push(`${pronoun[i]}${adj[j]}${noun[k]}${ext[l]}`);
+            }
+        }
+    }
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+console.log(dominios.join('\n'));
+
+function domain_name_generator() {
+    const first = pronoun[Math.floor(Math.random() * pronoun.length)];
+    const second = adj[Math.floor(Math.random() * adj.length)];
+    const third = noun[Math.floor(Math.random() * noun.length)];
+    const fourth = ext[Math.floor(Math.random() * ext.length)];
+
+    const domain_name = `${first}${second}${third}${fourth}`;
+    document.getElementById("domain").value = domain_name;
+    
+    console.log(domain_name);
+}
